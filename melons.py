f77a2d0fa6192852f98393d6137a42801d5757fc
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-13 11:54:16 -0700

    Created AbstractMelonOrder parent class and refactored children

---
+++ melons.py
@@ -0,0 +1,51 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder(object):
+    """An abstract base class that other Melon Orders inherit from."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    order_type = "domestic"
+    tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    order_type = "international"
+    tax = 0.17
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes."""
+
+        self.country_code = country_code
+        return super(InternationalMelonOrder, self).__init__(species, qty)
+
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
