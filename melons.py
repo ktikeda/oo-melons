7d9db4cf6c52b1f96cc3f39c89305fa6bf169067
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-13 12:45:51 -0700

    Made self.pass_inspection an instance attribute, changed order_type from attribute to argument, and made GovernmentMelonOrder a subclass of DomesticMelonOrder

--- melons.py
+++ melons.py
@@ -4,12 +4,13 @@
 class AbstractMelonOrder(object):
     """An abstract base class that other Melon Orders inherit from."""
 
-    def __init__(self, species, qty):
+    def __init__(self, species, qty, order_type):
         """Initialize melon order attributes."""
 
         self.species = species
         self.qty = qty
         self.shipped = False
+        self.order_type = order_type
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -34,26 +35,22 @@ class AbstractMelonOrder(object):
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
-    order_type = "domestic"
-
     def __init__(self, species, qty, tax=0.08):
         """Initialize melon order attributes."""
 
         self.tax = tax
-        return super(DomesticMelonOrder, self).__init__(species, qty)
+        return super(DomesticMelonOrder, self).__init__(species, qty, 'domestic')
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
-
-    order_type = "international"
 
     def __init__(self, species, qty, country_code, tax=0.17):
         """Initialize melon order attributes."""
 
         self.country_code = country_code
         self.tax = tax
-        return super(InternationalMelonOrder, self).__init__(species, qty)
+        return super(InternationalMelonOrder, self).__init__(species, qty, 'international')
 
     def get_country_code(self):
         """Return the country code."""
@@ -61,16 +58,12 @@ class InternationalMelonOrder(AbstractMelonOrder):
         return self.country_code
 
 
-class GovernmentMelonOrder(AbstractMelonOrder):
+class GovernmentMelonOrder(DomesticMelonOrder):
     """A melon order for the US Government"""
 
-    passed_inspection = False
-    order_type = 'domestic'
-
     def __init__(self, species, qty, tax=0):
-        self.tax = tax
-
-        return super(GovernmentMelonOrder, self).__init__(species, qty)
+        self.passed_inspection = False
+        return super(GovernmentMelonOrder, self).__init__(species, qty, tax)
 
     def mark_inspection(self, passed):
 
